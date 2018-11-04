4f2656ae2ce3ff75028c395eb982d7ce4c2aa196
Paul Anthony McGowan <anthonymcgowan1@gmail.com>
2018-11-04 11:23:33 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -153,7 +153,25 @@ function twentynineteen_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'twentynineteen_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'twentynineteen_content_width', 0 );
+/*
+*Add Woocommerce support
+*/
+function twentynineteen_add_woocommerce_support() {
+	add_theme_support( 'woocommerce', array(
+		'thumbnail_image_width' => 150,
+		'single_image_width'    => 300,
 
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+	) );
+}
+add_action( 'after_setup_theme', 'twentynineteen_add_woocommerce_support' );
 /**
  * Enqueue scripts and styles.
  */
